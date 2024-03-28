c9f9e074efecd5ad4ca030ce8b8e7b0eba8729ec
Abhishek-Zakhariya <abhishek.zakhariya.2024@gmail.com>
2024-03-28 19:40:54 +0530

    Added CRUD operation

--- route.js
+++ route.js
@@ -220,26 +220,343 @@ function route(app, conn, md5) {
         });
     });
 
-    function generateSalt() {
-        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    app.get('/Crud', (req, res) => {
+        res.render('./CRUD/form');
+    })
 
-        let salt = '';
-        const charactersLength = characters.length;
-        for (let i = 0; i < 5; i++) {
-            salt += characters.charAt(Math.floor(Math.random() * charactersLength));
+    app.post('/insertdata', (req, res) => {
+
+        let fname = req.body.fname;
+        let lname = req.body.lname;
+        let desg = req.body.designation;
+        let gender = req.body.gender;
+        let relationship = req.body.Relationship;
+        let email = req.body.email;
+        let phn_no = req.body.phnno;
+        let addr1 = req.body.add1;
+        let addr2 = req.body.add2;
+        let city = req.body.city;
+        let state = req.body.state;
+        let dob = req.body.dob;
+
+        var sql = `insert into basic_detail (fname,lname,designation,gender,dob,relationship,email,phn_no,addr1,addr2,city,state) values ("${fname}","${lname}","${desg}","${gender}",str_to_date('${dob}','%d/%m/%Y'),"${relationship}","${email}",${phn_no},"${addr1}","${addr2}","${city}","${state}")`;
+
+        var lastId;
+
+        conn.query(sql, async function (err, result) {
+            if (err) throw err;
+            console.log("Basic Record Inserted Successfully..");
+            EducationTable(req, res, result.insertId);
+            ExperianceTable(req, res, result.insertId);
+            ReferenceTable(req, res, result.insertId);
+            PreferenceTable(req, res, result.insertId);
+            (req.body.Language) ? LanguageTable(req, res, result.insertId) : '';
+            (req.body.tech) ? TechTable(req, res, result.insertId) : '';
+            res.render('./CRUD/form', { lastId: result.insertId });
+        })
+    })
+
+    function EducationTable(req, res, emp_id) {
+        console.log(req.body);
+        var edu_sql;
+        let edu_data = req.body;
+        let coursename = req.body.coursename;
+        let board = req.body.board;
+        let pass_year = req.body.pass_year;
+        let percentage = req.body.percentage;
+
+        for (let i = 0; i < edu_data.board.length; i++) {
+
+            if (coursename[i] == '' && board[i] == '' && pass_year[i] == '' && percentage[i] == '') {
+                continue;
+            }
+            edu_sql = `insert into education_detail(emp_id,course_name,board_or_uni,pass_year,percentage)values(${emp_id},"${coursename[i]}","${board[i]}",${pass_year[i]},${percentage[i]})`;
+
+            conn.query(edu_sql, function (err, result) {
+                if (err) throw err;
+                console.log("Education Record Inserted Successfully..");
+            })
         }
-        return salt;
     }
 
-    function generateLink() {
-        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    function ExperianceTable(req, res, emp_id) {
+        var exp_sql;
+        let cmpname = req.body.cmpnname;
+        let designation = req.body.desig;
+        let from_date = req.body.start_date;
+        let to_date = req.body.end_date;
 
-        let key = '';
-        const charactersLength = characters.length;
-        for (let i = 0; i < 12; i++) {
-            key += characters.charAt(Math.floor(Math.random() * charactersLength));
+        for (let i = 0; i < cmpname.length; i++) {
+
+            if (cmpname[i] == '' && designation[i] == '' && from_date[i] == '' && to_date[i] == '') {
+                continue;
+            }
+
+            exp_sql = `insert into experience_detail(emp_id,cmp_name,designation,from_date,to_date)values(${emp_id},"${cmpname[i]}","${designation[i]}",str_to_date('${from_date[i]}','%d/%m/%Y'),str_to_date('${to_date[i]}','%d/%m/%Y'))`;
+
+            conn.query(exp_sql, function (err, result) {
+                if (err) throw err;
+                console.log("Experience Record Inserted Successfully..");
+            })
         }
-        return key;
     }
+
+    function ReferenceTable(req, res, emp_id) {
+        var ref_sql;
+        let name = req.body.name;
+        let cont = req.body.contact;
+        let relation = req.body.rel;
+        for (let i = 0; i < name.length; i++) {
+
+            if (name[i] == '' && cont[i] == '' && relation[i] == '') {
+                continue;
+            }
+
+            ref_sql = `insert into reference_detail(emp_id,name,contact,relation)values(${emp_id},"${name[i]}",${cont[i]},'${relation[i]}')`;
+
+            conn.query(ref_sql, function (err, result) {
+                if (err) throw err;
+                console.log("Reference Record Inserted Successfully..");
+            })
+        }
+    }
+
+    function PreferenceTable(req, res, emp_id) {
+        var pre_sql;
+        let location = req.body.loc;
+        let notice = req.body.notice;
+        let exp_ctc = req.body.exp_ctc;
+        let cur_ctc = req.body.cur_ctc;
+        let department = req.body.dept;
+
+        var pre_sql = `insert into preference_detail(emp_id,location,notice_period,exp_ctc,cur_ctc,dept) values (${emp_id},"${location}","${notice}",${exp_ctc},${cur_ctc},"${department}")`;
+
+        conn.query(pre_sql, function (err, result) {
+            if (err) throw err;
+            console.log("Preference Records Inserted Successfully..");
+        })
+    }
+
+    function LanguageTable(req, res, emp_id) {
+        var lang_sql;
+        let language = req.body.Language;
+        let hindi = req.body.hindi;
+        let english = req.body.english;
+        let gujarati = req.body.gujarati;
+
+        console.log("Enter Language");
+        let value = [];
+        for (let i = 0; i < language.length; i++) {
+            if (language[i] == 'hindi') {
+                for (let j = 0; j < hindi.length; j++) {
+                    value.push(`(${emp_id},'${language[i]}','${hindi[j]}')`);
+                }
+            }
+            if (language[i] == 'english') {
+                for (let j = 0; j < english.length; j++) {
+                    value.push(`(${emp_id},'${language[i]}','${english[j]}')`);
+                }
+            }
+            if (language[i] == 'gujarati') {
+                for (let j = 0; j < gujarati.length; j++) {
+                    value.push(`(${emp_id},'${language[i]}','${gujarati[j]}')`);
+                }
+            }
+        }
+        let str = value.join(",")
+        lang_sql = `insert into language_detail(emp_id,language,ability) values ${str}`;
+
+        conn.query(lang_sql, function (err, result) {
+            if (err) throw err;
+            console.log("Language Record Inserted Successfully..");
+        })
+    }
+
+    function TechTable(req, res, emp_id) {
+
+        var tech_sql;
+        let skill;
+        let tech = req.body.tech;
+        let skill1 = req.body.skill1;
+        let skill2 = req.body.skill2;
+        let skill3 = req.body.skill3;
+        let skill4 = req.body.skill4;
+
+        let value = [];
+
+        for (let i = 0; i < tech.length; i++) {
+            if (tech[i] == 'php') {
+                value.push(`(${emp_id},'${tech[i]}','${skill1}')`);
+            }
+            if (tech[i] == 'mysql') {
+                value.push(`(${emp_id},'${tech[i]}','${skill2}')`);
+            }
+            if (tech[i] == 'laravel') {
+                value.push(`(${emp_id},'${tech[i]}','${skill3}')`);
+            }
+            if (tech[i] == 'oracle') {
+                value.push(`(${emp_id},'${tech[i]}','${skill4}')`);
+            }
+        }
+        let str = value.join(",")
+        tech_sql = `insert into techno_detail (emp_id,technology,proficiency) values ${str}`;
+
+        conn.query(tech_sql, function (err, result) {
+            if (err) throw err;
+            console.log("Technology Record Inserted Successfully..");
+        })
+    }
+
+    app.get('/edit', async (req, res) => {
+
+        let result = await fetchBasicDetail(req, res);
+        let education = await fetch_EducationData(req, res);
+        let experience = await fetchExperienceData(req, res);
+        let language = await fetchLanguageData(req, res);
+        let reference = await fetchReferenceData(req, res);
+        let preference = await fetchpreferenceData(req, res);
+        res.render('./CRUD/update', { result: result[0], education, experience, language, reference, preference });
+
+    })
+
+    async function fetchBasicDetail(req, res) {
+        let sql = `select * from basic_detail where emp_id = 4`;
+
+        const promise = new Promise(function (myResolve, myReject) {
+            conn.query(sql, function (err, result) {
+                if (err) throw err;
+                myResolve(result);
+            })
+        })
+        return await promise.then(function (result) {
+            return result;
+        })
+    }
+
+    async function fetch_EducationData(req, res) {
+        let edu_sql = `select * from education_detail where emp_id = 4`;
+
+        const promise = new Promise(function (myResolve, myReject) {
+            conn.query(edu_sql, function (err, result) {
+                if (err) throw err;
+                myResolve(result)
+            })
+        })
+        return await promise.then(function (result) {
+            return result
+        })
+    }
+
+    async function fetchExperienceData(req, res) {
+        let exp_sql = `select * from experience_detail where emp_id = 4`;
+
+        const promise = new Promise(function (myResolve, myReject) {
+            conn.query(exp_sql, function (err, result) {
+                if (err) throw err;
+                myResolve(result)
+            })
+        })
+        return await promise.then(function (result) {
+            return result
+        })
+    }
+
+    async function fetchLanguageData(req, res) {
+        let lang_sql = `select * from language_detail where emp_id = 4`;
+
+        const promise = new Promise(function (myResolve, myReject) {
+            conn.query(lang_sql, function (err, result) {
+                if (err) throw err;
+                myResolve(result)
+            })
+        })
+        return await promise.then(function (result) {
+            return result
+        })
+    }
+
+    async function fetchReferenceData(req, res) {
+        let ref_sql = `select * from reference_detail where emp_id = 4`;
+
+        const promise = new Promise(function (myResolve, myReject) {
+            conn.query(ref_sql, function (err, result) {
+                if (err) throw err;
+                myResolve(result)
+            })
+        })
+        return await promise.then(function (result) {
+            return result;
+        })
+    }
+
+    async function fetchpreferenceData(req, res) {
+        let pref_sql = `select * from preference_detail where emp_id = 4`;
+
+        const promise = new Promise(function (myResolve, myReject) {
+            conn.query(pref_sql, function (err, result) {
+                if (err) throw err;
+                myResolve(result)
+            })
+        })
+        return await promise.then(function (result) {
+            return result;
+        })
+    }
+
+    app.post('/edit', (req, res) => {
+        let fname = req.body.fname;
+        let lname = req.body.lname;
+        let desg = req.body.designation;
+        let gender = req.body.gender;
+        let relationship = req.body.Relationship;
+        let email = req.body.email;
+        let phn_no = req.body.phnno;
+        let addr1 = req.body.add1;
+        let addr2 = req.body.add2;
+        let city = req.body.city;
+        let state = req.body.state;
+        let dob = req.body.dob;
+
+        var sql = `update basic_detail set fname = "${fname}",lname =" ${lname}",designation = "${desg}",
+        gender = "${gender}",dob = "${dob}",relationship = "${relationship}",email = "${email}",phn_no = ${phn_no},addr1 = "${addr1}",addr2 = "${addr2}",city = "${city}", state = "${state}"`;
+
+        var lastId;
+
+        conn.query(sql, async function (err, result) {
+            if (err) throw err;
+            console.log("Basic Record Updated Successfully..");
+            // EducationTable(req, res, result.insertId);
+            // ExperianceTable(req, res, result.insertId);
+            // ReferenceTable(req, res, result.insertId);
+            // PreferenceTable(req, res, result.insertId);
+            // (req.body.Language) ? LanguageTable(req, res, result.insertId) : '';
+            // (req.body.tech) ? TechTable(req, res, result.insertId) : '';
+            res.render('./CRUD/form');
+        })
+
+    })
 }
+
+function generateSalt() {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    let salt = '';
+    const charactersLength = characters.length;
+    for (let i = 0; i < 5; i++) {
+        salt += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return salt;
+}
+
+function generateLink() {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    let key = '';
+    const charactersLength = characters.length;
+    for (let i = 0; i < 12; i++) {
+        key += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return key;
+}
+
 module.exports = route;
